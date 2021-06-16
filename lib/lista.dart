ac1ce2cb1a9e7e5c528a9258412134c63316935d
Juan Pablo Oviedo <8oviedopablojuan@gmail.com>
2021-06-16 20:39:49 -0300

    alteraçao de comentarios

--- lib/lista.dart
+++ lib/lista.dart
@@ -18,7 +18,7 @@ class _ListaState extends State<Lista> {
   bool loading = false; //* imagen animada de loading
 
   @override
-  void initState() { //* initState(), se usa para iniciar el primer metodo. llamo la api y cargo los poke antes de cualquier cosa
+  void initState() { //* initState(), se usa para iniciar el primer metodo. llamo la api y cargo los poke antes de cualquier cosa. 
     cargarPokemon();
     super.initState();
   }
